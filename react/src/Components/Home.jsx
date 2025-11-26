4056d0cd35b03b4c2bbd7c81f1e27f9313e9a954
Snehaparthipan <snehaparthipan123@gmail.com>
2025-11-26 15:32:12 +0000

    create home content

--- react/src/Components/Home.jsx
+++ react/src/Components/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+
+
+const data = [
+  { icon: <i className="fa-solid fa-calendar-days"></i>, title: "45-day move-in guarantee" },
+  { icon: <i className="fa-solid fa-clipboard"></i>, title: "146 quality checks" },
+  { icon: <i className="fa-solid fa-heart"></i>, title: "1,00,000+ happy homes" },
+  { icon: <i className="fa-solid fa-city"></i>, title: "100+ cities" },
+];
 
 
 export default function Home() {
   return (
-    <>
-    {/* <video src={banvid} muted autoPlay loop playsInline className='banvideo'></video> */}
-    </>
-  )
+    <div className="body">
+    <div className="ban">
+        <img src="https://i.postimg.cc/tCcjNdV6/img4.jpg" alt="" />
+    </div>
+    <div className="whycontainer">
+      <h2>Why choose us</h2>
+
+      <div className="why-cards">
+        {data.map((item, index) => (
+          <div className="why-card" key={index}>
+            <div className="why-icon">{item.icon}</div>
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
+    </div></div>
+    
+  );
 }

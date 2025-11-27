a948e257d9ee2da14298214f6ff5a1384a14ea70
Menaga <menagashanmugam02@gmail.com>
2025-11-27 18:42:42 +0530

    changes

--- react/src/Components/Bedroom.jsx
+++ react/src/Components/Bedroom.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export default function Bedroom() {
+
+export default function Hero() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShow(true), 200);
+  }, []);
+
   return (
-    <div>
+    <>
+      <div className="topBannerImage">
+        <img
+          src="https://i.postimg.cc/tgqg91MQ/bed-hero.jpg"
+          alt=""
+          style={{ width: "100%", height: "610px" }}
+        />
+        <div className="topBannerTextBox">
+          <h1>
+            Customized Bedroom <br />
+            For Your Dream Home
+          </h1>
+        </div>
+      </div>
+
+      <div className="islandShowcase">
+        <div className="islandImgGroup">
+          <img
+            src="https://i.postimg.cc/tgqg91MQ/bed-hero.jpg"
+            alt=""
+          />
+          <img
+            src="https://i.postimg.cc/tgqg91MQ/bed-hero.jpg"
+            alt=""
+          />
+        </div>
+
+        <div className="islandInfoBox">
+          <h2>Bed</h2>
+          <p>
+            Relax with custom-made ideas of cots from NESTIFY. Bedroom beauty
+            depends mainly on how the cot is built and arranged, supported by
+            lighting, and side tables. Choose from the wide range of styles and
+            colour options available in your desired design. A neat storage,
+            peace of mind style and comfort can be expected when it designed as
+            required.
+          </p>
+
+          <button className="viewMoreBtn">view More</button>
+        </div>
+      </div>
+      <div className="islandShowcase">
+        <div className="islandInfoBox">
+          <h2>Dressing Unit</h2>
+          <p>
+            Dressing units are an essential part of the bedroom interior, it
+            often comes integrated into the wardrobes. NESTIFY offers you the
+            choice to build as per your requirements in custom style and
+            measurements. Customers can get the dressing table as per your
+            requirements anywhere in India as part of your bedroom interior.
+          </p>
+
+          <button className="viewMoreBtn">view More</button>
+        </div>
+        <div className="islandImgGroup">
+          <img
+            src="https://i.postimg.cc/tgqg91MQ/bed-hero.jpg"
+            alt=""
+          />
+          <img
+            src="https://i.postimg.cc/tgqg91MQ/bed-hero.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="islandShowcase">
+        <div className="islandImgGroup">
+          <img src="https://i.postimg.cc/tgqg91MQ/bed-hero.jpg" alt=" " />
+          <img src="https://i.postimg.cc/tgqg91MQ/bed-hero.jpg" alt="" />
+        </div>
+
+        <div className="islandInfoBox">
+          <h2>Wardrobe</h2>
+          <p>
+            Classy and trendy wardrobes transform a bedroom in addition to give
+            ample storage facilities. Where it fits better and what should be
+            the size is often a matter to be discussed with a designer and
+            decide. Next is the choice of color and finish to match with the
+            theme of the bedroom.
+          </p>
+
+          <button className="viewMoreBtn">view More</button>
+        </div>
+      </div>
       
-    </div>
-  )
+    </>
+  );
 }
--- react/src/Components/Livingroom.jsx
+++ react/src/Components/Livingroom.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export default function Livingroom() {
+
+export default function Hero() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShow(true), 200);
+  }, []);
+
   return (
-    <div>
+    <>
       
-    </div>
-  )
+      <div className="topBannerImage">
+        <img
+          src="https://i.postimg.cc/ZRyzm44K/island2.jpg"
+          alt=""
+          style={{ width: "100%", height: "610px" }}
+        />
+        <div className="topBannerTextBox">
+          <h1>
+            Design the Heart of Your Home <br />
+            With a Kitchen That Reflects You
+          </h1>
+        </div>
+      </div>
+
+     
+
+      <div className="islandShowcase">
+        <div className="islandImgGroup">
+          <img src="https://i.postimg.cc/ZRyzm44K/island2.jpg" alt="Island 1" />
+          <img src="https://i.postimg.cc/ZRyzm44K/island2.jpg" alt="Island 2" />
+        </div>
+
+        <div className="islandInfoBox">
+          <h2>Island Kitchen</h2>
+          <p>
+            Island kitchen series of modern designs from NESTIFY offers you a
+            wide range of selections to help the customer get the best idea.
+            These are different from the traditional style and towards the new
+            trends.
+          </p>
+
+          <button className="viewMoreBtn">view More</button>
+        </div>
+      </div>
+      <div className="islandShowcase">
+        <div className="islandInfoBox">
+          <h2>L Shape Kitchen</h2>
+          <p>
+            L-shaped kitchens from NESTIFY are 100% customized to fit the space.
+            Various options are given below which can further be modified and
+            made as per a design finalized.
+          </p>
+
+          <button className="viewMoreBtn">view More</button>
+        </div>
+        <div className="islandImgGroup">
+          <img src="https://i.postimg.cc/ZRyzm44K/island2.jpg" alt="Island 1" />
+          <img src="https://i.postimg.cc/ZRyzm44K/island2.jpg" alt="Island 2" />
+        </div>
+      </div>
+      <div className="islandShowcase">
+        <div className="islandImgGroup">
+          <img src="https://i.postimg.cc/ZRyzm44K/island2.jpg" alt="Island 1" />
+          <img src="https://i.postimg.cc/ZRyzm44K/island2.jpg" alt="Island 2" />
+        </div>
+
+        <div className="islandInfoBox">
+          <h2>Parallel Kitchen</h2>
+          <p>
+            Parallel Kitchen Parallel kitchen is the concept of making cabinets
+            on both the longer sides, especially as per the suitability to the
+            space measured. Our interior designer would be able to help you
+            decide the shape, color, and finish by giving suggestions.
+          </p>
+
+          <button className="viewMoreBtn">view More</button>
+        </div>
+      </div>
+      <div className="islandShowcase">
+        <div className="islandInfoBox">
+          <h2>Straight Kitchen</h2>
+          <p>
+            Big space can accommodate a straight kitchen with differences from
+            commonly used styles. Best of the efforts are made by our team of
+            interior designers to provide unique ideas and concepts in making
+            modular kitchens to suit the space and match with the house.
+          </p>
+
+          <button className="viewMoreBtn">view More</button>
+        </div>
+        <div className="islandImgGroup">
+          <img src="https://i.postimg.cc/ZRyzm44K/island2.jpg" alt="Island 1" />
+          <img src="https://i.postimg.cc/ZRyzm44K/island2.jpg" alt="Island 2" />
+        </div>
+      </div>
+    </>
+  );
 }

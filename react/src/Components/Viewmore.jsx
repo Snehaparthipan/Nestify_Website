b852694c0bd4e160d5985590a04efe905461e1f3
Menaga <menagashanmugam02@gmail.com>
2025-11-28 09:24:24 +0530

    viewmore

--- react/src/Components/Viewmore.jsx
+++ react/src/Components/Viewmore.jsx
@@ -1,48 +1,60 @@
-import React, { useEffect, useState, useContext } from "react";
-import { CartContext } from "./CartContext.jsx";
+// import React, { useEffect, useState, useContext } from "react";
+// import { CartContext } from "./CartContext.jsx";
 
-export default function IslandKitchen() {
-  const [kitchens, setKitchens] = useState([]);
-  const { addToCart } = useContext(CartContext);
+// export default function IslandKitchen() {
+//   const [kitchens, setKitchens] = useState([]);
+//   const { addToCart } = useContext(CartContext);
 
-  useEffect(() => {
-    const data = [
-      { name: "Kitchen", img: "https://i.postimg.cc/ZRyzm44K/island2.jpg" },
-      { name: "Kitchen", img: "https://i.postimg.cc/ZRyzm44K/island2.jpg" },
-      { name: "Kitchen", img: "https://i.postimg.cc/ZRyzm44K/island2.jpg" },
-      { name: "Kitchen", img: "https://i.postimg.cc/ZRyzm44K/island2.jpg" },
-    ];
-    setKitchens(data);
-  }, []);
+//   useEffect(() => {
+//     const data = [
+//       { name: "Kitchen", img: "https://i.postimg.cc/ZRyzm44K/island2.jpg" },
+//       { name: "Kitchen", img: "https://i.postimg.cc/ZRyzm44K/island2.jpg" },
+//       { name: "Kitchen", img: "https://i.postimg.cc/ZRyzm44K/island2.jpg" },
+//       { name: "Kitchen", img: "https://i.postimg.cc/ZRyzm44K/island2.jpg" },
+//     ];
+//     setKitchens(data);
+//   }, []);
 
-  const handleAdd = (item) => {
-    addToCart(item); 
-    alert("Added to Cart!");
-  };
+//   const handleAdd = (item) => {
+//     addToCart(item); 
+//     alert("Added to Cart!");
+//   };
 
+//   return (
+//     <div className="island-container">
+//       <h1 className="title">Island Kitchen</h1>
+
+//       <p className="description">
+//         Kitchens should be elegant looking as well as serve our purpose of
+//         storage, convenient cooking, and comfortable living. Below are some
+//         island kitchen models displayed for inspiration.
+//       </p>
+
+//       <div className="kitchen-grid">
+//         {kitchens.map((item, index) => (
+//           <div key={index} className="kitchen-card">
+//             <img src={item.img} className="kitchen-img" />
+
+//             <div className="kitchen-name">{item.name}</div>
+
+//             <button className="add-btn" onClick={() => handleAdd(item)}>
+//               Add Design
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+import React from 'react'
+
+export default function Viewmore() {
   return (
-    <div className="island-container">
-      <h1 className="title">Island Kitchen</h1>
-
-      <p className="description">
-        Kitchens should be elegant looking as well as serve our purpose of
-        storage, convenient cooking, and comfortable living. Below are some
-        island kitchen models displayed for inspiration.
-      </p>
-
-      <div className="kitchen-grid">
-        {kitchens.map((item, index) => (
-          <div key={index} className="kitchen-card">
-            <img src={item.img} className="kitchen-img" />
-
-            <div className="kitchen-name">{item.name}</div>
-
-            <button className="add-btn" onClick={() => handleAdd(item)}>
-              Add Design
-            </button>
-          </div>
-        ))}
-      </div>
+    <div>
+      
     </div>
-  );
+  )
 }
+
+
